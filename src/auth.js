20979be204f7a3f2e6f061f9474f223642f9941c
Anil Lingolu <71059714+anil135@users.noreply.github.com>
2026-05-09 00:57:49 +0530

    Create auth.js

---
+++ src/auth.js
@@ -0,0 +1,23 @@
+import config from "./cognitoConfig";
+
+export function login() {
+  const loginUrl =
+    `${config.domain}/login?` +
+    `client_id=${config.clientId}` +
+    `&response_type=${config.responseType}` +
+    `&scope=${config.scope}` +
+    `&redirect_uri=${config.redirectUri}`;
+
+  window.location.href = loginUrl;
+}
+
+export function logout() {
+  localStorage.clear();
+
+  const logoutUrl =
+    `${config.domain}/logout?` +
+    `client_id=${config.clientId}` +
+    `&logout_uri=${config.redirectUri}`;
+
+  window.location.href = logoutUrl;
+}
