d81412e835d01290f8137890d30c9e39daaee1bf
Anil Lingolu <71059714+anil135@users.noreply.github.com>
2026-05-09 01:14:38 +0530

    Update auth.js

--- src/auth.js
+++ src/auth.js
@@ -1,23 +1,25 @@
-import config from "./cognitoConfig";
+import config from './cognitoConfig'
 
 export function login() {
+
   const loginUrl =
     `${config.domain}/login?` +
     `client_id=${config.clientId}` +
     `&response_type=${config.responseType}` +
     `&scope=${config.scope}` +
-    `&redirect_uri=${config.redirectUri}`;
+    `&redirect_uri=${config.redirectUri}`
 
-  window.location.href = loginUrl;
+  window.location.href = loginUrl
 }
 
 export function logout() {
-  localStorage.clear();
+
+  localStorage.clear()
 
   const logoutUrl =
     `${config.domain}/logout?` +
     `client_id=${config.clientId}` +
-    `&logout_uri=${config.redirectUri}`;
+    `&logout_uri=${config.redirectUri}`
 
-  window.location.href = logoutUrl;
+  window.location.href = logoutUrl
 }
