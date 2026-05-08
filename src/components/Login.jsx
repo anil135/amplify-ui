654336e43eed99f401ddad587d6bbdfc1c31ab48
Anil Lingolu <71059714+anil135@users.noreply.github.com>
2026-05-09 01:16:22 +0530

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,24 +1,16 @@
-import { login } from "../auth";
+import { login } from '../auth'
 
 export default function Login() {
+
   return (
-    <div style={styles.container}>
+    <div className="login-container">
+
       <h1>Alta Video Retrieval</h1>
 
       <button onClick={login}>
         Login with Cognito
       </button>
-    </div>
-  );
-}
 
-const styles = {
-  container: {
-    height: "100vh",
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center",
-    gap: 20
-  }
-};
+    </div>
+  )
+}
